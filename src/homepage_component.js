717ee62d0d95232c8078d32d81b9c9da9d37871f
shanesharareh <shane.s.sharareh@gmail.com>
2021-06-07 13:49:55 -0700

    add homepage and directory components

---
+++ src/homepage_component.js
@@ -0,0 +1,43 @@
+import react from 'react'; 
+import './homepage_styles.scss'
+const HomePage = () => (
+    <div className="directory-menu">
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">HATS</h1>
+                <span className="title">SHOP NOW</span>
+
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">JACEKTS</h1>
+                <span className="title">SHOP NOW</span>
+
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">SNEAKERS</h1>
+                <span className="title">SHOP NOW</span>
+
+            </div>
+        </div>
+
+          <div className="menu-item">
+            <div className="content">
+                <h1 className="title">MENS</h1>
+                <span className="title">SHOP NOW</span>
+
+            </div>
+        </div>
+          <div className="menu-item">
+            <div className="content">
+                <h1 className="title">WOMENS</h1>
+                <span className="title">SHOP NOW</span>
+
+            </div>
+        </div>
+    </div>
+)
+export default HomePage;
---
+++ src/pages/homepage/homepage_component.jsx
@@ -0,0 +1,9 @@
+import react from 'react'; 
+import './homepage_styles.scss'
+import Directory from '../../components/directory/directory_component'
+const HomePage = () => (
+   <div className="homepage">
+       <Directory/>
+    </div>
+)
+export default HomePage;
